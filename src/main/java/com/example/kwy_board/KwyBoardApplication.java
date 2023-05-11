c3536d6a5ea03617273cf1815ec1fd232a22d78f
micheal1woo <kjw8922@naver.com>
2023-05-11 10:57:38 +0900

    first commit

---
+++ src/main/java/com/example/kwy_board/KwyBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.kwy_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwyBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KwyBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kwy_board/KwyBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kwy_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwyBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
